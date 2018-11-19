00fd0f630789c0168e24b9bb892c2d89f35ff6e9
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2018-11-19 19:31:47 +0200

    js object

--- JS:From_Fundamentals_to_Functional_JS/Objects/objects.js
+++ JS:From_Fundamentals_to_Functional_JS/Objects/objects.js
@@ -5,21 +5,20 @@ function logConsole(value) {
 function logTable(value) {
   console.log(value);
 }
-
 const person = {};
-person.name = "mohammedelzanaty";
+person.name = 'mohammedelzanaty';
 let who = person.name; // mohammedelzanaty
 // person.0 // it will an error
-person[0] = "hello number";
-const plea = "wouldShe";
-person[plea] = "I would never";
+person[0] = 'hello number';
+const plea = 'wouldShe';
+person[plea] = 'I would never';
 logTable(person); // { '0': 'hello number', name: 'mohammedelzanaty', wouldShe: 'I would never' }
-person["plea"] = "I would never";
+person['plea'] = 'I would never';
 logTable(person); // { '0': 'hello number', name: 'mohammedelzanaty', wouldShe: 'I would never', plea: 'I would never' }
 
 const arr = [];
-arr.name = "hello";
-arr[1] = "znzn";
+arr.name = 'hello';
+arr[1] = 'znzn';
 logConsole(arr); // [ <1 empty item>, 'znzn', name: 'hello' ]
 logConsole(Array.isArray(arr)); // true
 logConsole(arr.length); // 2
